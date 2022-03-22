0b2a7cc1fe19852f11a7647792fdccb26fa93db3
Sahil <aggarwalsahil2004@gmail.com>
2022-03-22 18:10:02 +0530

    Netlify release testing 4

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import AOS from "aos";
 
 function App() {
 	useEffect(() => {
-		AOS.init();
+		AOS.init({ once: true, offset: 20 });
 		AOS.refresh();
 	}, [])
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/pages//*.{js,jsx,ts,tsx}", "./src/components//*.{js,jsx,ts,tsx}"],
+  content: ["./src/pages//*.{js,jsx,ts,tsx}", "./src/components//*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     screens: {
       sm: '640px',
