b6c1804b40c7770c890af6a4b5952680180e803b
Sahil <aggarwalsahil2004@gmail.com>
2022-03-23 23:11:31 +0530

    test commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import bg from '../media/bg.webp'
 import logo from '../media/logo.webp'
+import logo2 from '../media/logo-white.webp'
 
 export default function Welcome() {
   const redirect = useNavigate();
@@ -19,7 +20,8 @@ export default function Welcome() {
     <div className={`flex container fixed inset-0 normal:text-white normal:text-opacity-90 justify-center min-w-full ${welcome ? '' : 'hidden'}`}>
       <div className="container col-span-3 flex flex-col justify-evenly items-center min-w-[30vw] h-full normal:bg-purple-600">
         <div className='flex flex-col items-center'>
-          <img src={logo} alt="CloudNotes" id="logo" className='w-16 h-16 normal:invert-[90%]' />
+          <img src={logo} alt="CloudNotes" id="logo" className='w-16 h-16 normal:hidden' />
+          <img src={logo2} alt="CloudNotes" id="logo" className='w-16 h-16 invert-[10%] hidden normal:block' />
           <h1 className='text-xl font-bold normal:font-semibold'>CloudNotes - Notes on Cloud</h1>
         </div>
         <div className='px-7 text-justify'>CloudNotes is an online platform to save all your notes at one place on the cloud. It is a platform where your notes are totally encrypted and secured so that no one except you can access your notes.</div>
