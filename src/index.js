14a695b57e934b40eb5f3d533444fafb0bb7c0b1
Sahil <aggarwalsahil2004@gmail.com>
2022-03-23 20:23:57 +0530

    react-snap testing

--- src/index.js
+++ src/index.js
@@ -4,31 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // below code contains react-snap method which is a library that pre-renders an html page which is good for SEO and google bot now sees some content in our website.
-// const rootElement = document.getElementById("root");
-
-// if (rootElement.hasChildNodes()) {
-//   ReactDOM.hydrate(
-//     <React.StrictMode>
-//       <ToggleState>
-//         <App />
-//       </ToggleState>
-//     </React.StrictMode>, rootElement);
-// } else {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <ToggleState>
-//         <App />
-//       </ToggleState>
-//     </React.StrictMode>, rootElement);
-// }
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>, rootElement);
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
