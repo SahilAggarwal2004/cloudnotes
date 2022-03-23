d9cdd93255d3557b4531437283b51402365ab581
Sahil <aggarwalsahil2004@gmail.com>
2022-03-23 23:27:52 +0530

    rebuild

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import bg from '../media/bg.webp'
 import logo from '../media/logo.webp'
-// import logo2 from '../media/logo-white.webp'
 
 export default function Welcome() {
   const redirect = useNavigate();
