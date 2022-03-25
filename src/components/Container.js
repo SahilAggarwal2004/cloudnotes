b421c516d388c3f63d1551523de794d2f8386e40
Sahil <aggarwalsahil2004@gmail.com>
2022-03-25 11:19:11 +0530

    bug fixes

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Navbar from './Navbar';
 import Alert from './Alert';
 import Modal from './Modal';
-// const Navbar = lazy(() => import('./Navbar'));
-// const Alert = lazy(() => import('./Alert'));
-// const Modal = lazy(() => import('./Modal'));
 
 export default function Container() {
     const location = useLocation()
@@ -14,14 +10,6 @@ export default function Container() {
     return (
         location.pathname !== '/' ?
             <>
-                <HelmetProvider>
-                    <Helmet>
-                        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/fontawesome.css" />
-                        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/solid.css" />
-                        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/regular.css" />
-                        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/brands.css" />
-                    </Helmet>
-                </HelmetProvider>
                 <Navbar />
                 <Alert />
                 <Modal />
