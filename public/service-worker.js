649e4259e38c2959b6cc62ceea1f064e0d6515df
Sahil <aggarwalsahil2004@gmail.com>
2022-07-03 16:26:51 +0530

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
 // Listen for requests
 self.addEventListener('fetch', event => {
     event.respondWith(caches.match(event.request).then(async () => {
-        return fetch(event.request)
+        return fetch(event.request).catch(() => caches.match('index.html'))
         // .catch(() => caches.match('offline.html'))
     }))
 })
