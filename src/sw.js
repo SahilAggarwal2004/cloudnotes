9f88e75c79f3678e4285e5e1ed4f4f97cda2a3ee
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2022-08-26 00:48:55 +0530

    commit

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
 import { clientsClaim } from 'workbox-core'
 import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute, setDefaultHandler } from 'workbox-routing'
-import { CacheFirst, NetworkOnly } from 'workbox-strategies'
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
 import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { ExpirationPlugin } from 'workbox-expiration'
 import { offlineFallback } from 'workbox-recipes'
@@ -19,7 +19,7 @@ const urlsToCache = (self.__WB_MANIFEST || []).concat([
 ])
 precacheAndRoute(urlsToCache)
 
-setDefaultHandler(new NetworkOnly())
+setDefaultHandler(new StaleWhileRevalidate())
 offlineFallback({ pageFallback: '/offline' });
 
 registerRoute(({ request }) => request.destination === 'image', new CacheFirst({
