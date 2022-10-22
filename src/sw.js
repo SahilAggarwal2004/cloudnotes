a4b70b9e16a175875ba29687d25b172910701f7c
Sahil <aggarwalsahil2004@gmail.com>
2022-10-22 16:44:10 +0530

    sw update

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
 import { clientsClaim } from 'workbox-core'
 import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute, setDefaultHandler } from 'workbox-routing'
-import { CacheFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies'
+import { CacheFirst, NetworkFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies'
 import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { offlineFallback } from 'workbox-recipes'
 import { nanoid } from 'nanoid'
@@ -15,13 +15,19 @@ const urlsToCache = (self.__WB_MANIFEST || []).concat([
     { url: '/', revision },
     { url: '/dashboard', revision },
     { url: '/about', revision }
-])
+]).filter(({ url }) => url !== '/manifest.json')
 precacheAndRoute(urlsToCache)
 
 setDefaultHandler(new StaleWhileRevalidate())
 offlineFallback({ pageFallback: '/offline' });
 
 registerRoute(({ url }) => url.host === 'cloudnotes.onrender.com', new NetworkOnly())
+
+registerRoute(({ url }) => url.pathname === '/manifest.json', new NetworkFirst({
+    cacheName: 'manifest',
+    plugins: [new CacheableResponsePlugin({ statuses: [200] })]
+}))
+
 registerRoute(({ request }) => request.destination === 'image', new CacheFirst({
     cacheName: 'images',
     plugins: [new CacheableResponsePlugin({ statuses: [200] })]
