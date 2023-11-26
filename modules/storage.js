486f765b29854156d68a02058d4895fe959b3181
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-11-26 23:31:57 +0530

    commit

--- modules/storage.js
+++ modules/storage.js
@@ -5,7 +5,7 @@ export const setStorage = (key, value) => localStorage.setItem(key, JSON.stringi
 export const removeStorage = key => localStorage.removeItem(key)
 
 export const getStorage = (key, fallbackValue) => {
-    if (typeof localStorage === 'undefined') return;
+    if (typeof localStorage === 'undefined') return fallbackValue;
     let value = localStorage.getItem(key)
     try {
         if (!value) throw new Error("Value doesn't exist")
