177b1be0178f6d8c7db849c88c14519d12191659
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-10-17 14:18:32 +0530

    [fix]: redirect to dashboard after deleteing a note

--- src/pages/note/[noteId].jsx
+++ src/pages/note/[noteId].jsx
@@ -1,15 +1,18 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useMemo } from "react";
 import { useNoteContext } from "../../contexts/NoteProvider";
 import NoteItem from "../../components/NoteItem";
 
 export default function Note({ router }) {
   const { noteId } = router.query;
   const { notes } = useNoteContext();
-  const note = notes.find(({ _id }) => _id === noteId);
+  const note = useMemo(() => notes.find(({ _id }) => _id === noteId), [notes, noteId]);
 
-  return (
-    <div className="mt-5 px-3">
-      <NoteItem key={note._id} note={note} router={router} expanded />
-    </div>
-  );
+  useEffect(() => {
+    if (!note) router.push("/")
+  }, [note])
+
+  return note && <div className="mt-5 px-3">
+    <NoteItem key={note._id} note={note} router={router} expanded />
+  </div>
 }
