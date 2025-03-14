7c51c622985ab5b4284dc1b9119ae99ab1ce5291
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2025-03-14 17:35:23 +0530

    [chore]: update dependencies

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -29,7 +29,10 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     setLoading(false);
     init();
-    if ("serviceWorker" in navigator) window.serwist.register().then(handleVersionUpdate);
+    if ("serviceWorker" in navigator && window.serwist) {
+      window.serwist.register().then(() => window.serwist.addEventListener("controlling", handleVersionUpdate));
+      return () => window.serwist.removeEventListener("controlling", handleVersionUpdate);
+    }
   }, []);
 
   useEffect(() => {
